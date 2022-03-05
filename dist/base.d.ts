90b8dc3c381e337a6ad1dc9d2ff228aed07af7ce
cti1650 <cti1650@gmail.com>
2022-03-05 09:31:29 +0000

    テスト用スクリプト更新

--- dist/base.d.ts
+++ dist/base.d.ts
@@ -1,2 +1,3 @@
 #!/usr/bin/env node
 declare const argv: string[];
+declare const path: any;
--- src/base.ts
+++ src/base.ts
@@ -1,3 +1,23 @@
 #!/usr/bin/env node
 const argv = process.argv.slice(2);
 console.log(JSON.stringify(argv));
+
+const path = require('path');
+
+// プロセス ＝ nodeコマンドを叩いたプロセス
+// 実行ファイル ＝ nodeコマンドで叩かれたファイル
+// 現在のファイル ＝ 実行中のモジュールのファイル
+
+console.log('プロセスのカレントディレクトリ (1)', path.resolve());
+console.log('プロセスのカレントディレクトリ (2)', process.cwd());
+
+console.log('実行ファイルのパス', process.argv[1]);
+console.log('実行ファイルの名前', path.basename(process.argv[1]));
+console.log('実行ファイルのディレクトリ', path.dirname(process.argv[1]));
+
+console.log('現在のファイルのパス', __filename);
+console.log('現在のファイルの名前', path.basename(__filename));
+console.log('現在のファイルのディレクトリ', __dirname);
+
+console.log('現在のファイルの一つ上のディレクトリ', path.resolve(__dirname, '..'));
+console.log('現在のファイルの二つ上のディレクトリ', path.resolve(__dirname, '..', '..'));
