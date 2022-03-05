704bfd2fbaad935d929308aa147086de423b1064
cti1650 <cti1650@gmail.com>
2022-03-05 06:11:21 +0000

    テストコード追加

---
+++ dist/base.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+"use strict";
+const argv = process.argv.slice(2);
+console.log(JSON.stringify(argv));
---
+++ src/base.ts
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+const argv = process.argv.slice(2);
+console.log(JSON.stringify(argv));
